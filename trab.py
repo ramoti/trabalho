d11046429ff2da82b7c59069943bbbb610bc0553
Ramos <47822176+ramoti@users.noreply.github.com>
2019-12-02 19:48:37 -0300

    Update trab.py

--- trab.py
+++ trab.py
@@ -72,7 +72,7 @@ class Narrador (BaseModel):
 class Tecnico (BaseModel):
 
     nome = CharField ()
-    time = ForeignKeyField (Time)
+
 
     def __str__(self):
 
@@ -105,3 +105,41 @@ class Time (BaseModel):
     def __str__(self): 
 
         return self.jogadores + self.tecnico + self.nacionalidade
+
+if __name__ == "__main__":
+    
+    if os.path.exists(arq):
+        os.remove(arq)
+
+    try:
+        db.connect()
+        db.create_tables([Time,
+                          Jogador,
+                          Penalidade,
+                          Campeonato,
+                          Round,
+                          Partida,
+                          Narrador,
+                          Tecnico, 
+                          Premio,
+                          Pause,])
+    
+    except OperationalError as e:
+        print('erros: ' + str(e))
+
+    jogador1 = Jogador.create (nome = Ramos, idade = 18)
+    penalidade1 = Penalidade.create (numero_de_partidas = duas, nivel_penalidade = média)
+    premio1 = Premio.create (campeoes = doze milhoes , vice_campeoes = cinco milhoes)
+    campeonato1 = Campeonato.create (nome = superliga , nivel = nacional , premio = premio1)
+    round1 = Round.create (tempo = 2 mins , numero = três)
+    tecnico1 = Tecnico.create (nome = Hylson)
+    time1 = Time.create (jogadores = jogador1 , tecnico = tecnico1 , nacionalidade = brasileiro)
+    pausa1 = Pause.create (motivo = problemas tecnicos , tempo = 5 minutos)
+    narrador1 = Narrador.create (nome = sérgio , lingua = portugues )
+    partida1 = Partida.create (narrador = narrador1, rounds = round1 , times = time1 , jogadores = jogador1 , pausa=pausa1 , data = 20/5/2019)
+
+    print (partida1)
+    print ("")
+    print (campeonato1)
+    print ("")
+    print (penalidade1)
