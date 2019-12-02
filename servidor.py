f52c458adcb2f1f2a33a9dbea01d1d4845bc1f11
Ramos <47822176+ramoti@users.noreply.github.com>
2019-12-01 21:07:09 -0300

    Create servidor.py

---
+++ servidor.py
@@ -0,0 +1,45 @@
+from flask import Flask, json, jsonify
+from flask import request
+from hylson.py import Time, Penalidade, Campeonato, Partida
+from playhouse.shortcuts import model_to_dict
+
+app = Flask (__name__)
+
+@app.route ('/', methods = ['GET'])
+def main ():
+
+    return foi
+
+@app.route ('/listar_time')
+def listar_time ():
+
+    time = list(map(model_to_dict, Time.select()))
+    response = jsonify({"time": time})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route ('/listar_penalidade')
+def listar_penalidade ():
+
+    penalidade = list(map(model_to_dict, Penalidade.select()))
+    response = jsonify({"penalidade":penalidadee})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route ('/listar_campeonato')
+def listar_campeonato ():
+
+    campeonato = list(map(model_to_dict, Campeonato.select()))
+    response = jsonify({"campeonato": campeonato})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route ('/listar_partida')
+def listar_partida ():
+
+    partida = list(map(model_to_dict, Partida.select()))
+    response = jsonify({"partida": partida})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    
+app.run (debug=True, port =4999 )
